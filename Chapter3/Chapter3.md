9299d3e658e804943bfb4486cabd0335b0688ca3
FelixEbobo <thetemperennuke@yandex.ru>
2021-05-06 15:29:42 +0800

    Chapter3

--- Chapter3/Chapter3.md
+++
@@ -1,3 +0,0 @@
-# Краткий экскурс по командам
-
-### `docker build -t python_server .` - собрать образ из Dockerfile в текущей директории с именем *python_server*
---
+++ Chapter3/mysql.py
@@ -0,0 +1,38 @@
+import pymysql, uuid
+from random import randint
+import shlex
+
+connection_params = {
+    "user": "root",
+    "password": "123456",
+    "host": "127.0.0.1",
+    "port": 3122,
+}
+
+cnx = pymysql.Connect(**connection_params)
+
+cur = cnx.cursor()
+cur.execute("CREATE DATABASE IF NOT EXISTS isp;")
+cnx.select_db("isp")
+
+cur.execute("select @@version")
+output = cur.fetchall()
+print(output)
+
+cur.execute("CREATE TABLE IF NOT EXISTS person (person_id INTEGER PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20) NOT NULL, age INTEGER);")
+
+names = ["John", "Brohn", "James", "Ren", "Felix"]
+
+for name in names:
+    age = randint(15, 32)
+    cur.execute(f"INSERT INTO person (name, age) VALUES ('{name}', {age});")
+
+cnx.commit()
+
+cur.execute("SELECT * FROM person")
+output = cur.fetchall()
+
+for elem in output:
+    print(elem)
+
+cnx.close()
